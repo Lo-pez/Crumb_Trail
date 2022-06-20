3c845303d312b2a73291da86509e43e99eaeb0cd
Lo-pez <diego.lopez13@upr.edu>
2022-06-20 13:51:51 -0700

    Added sign up + bug fixes

--- app/src/main/java/com/example/crumbtrail/SignUpActivity.java
+++ app/src/main/java/com/example/crumbtrail/SignUpActivity.java
@@ -3,44 +3,77 @@ package com.example.crumbtrail;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
-
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.crumbtrail.data.model.User;
+import com.google.android.material.textfield.TextInputEditText;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import com.parse.SignUpCallback;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private ImageView back;
+    private Button signUp;
+    private TextInputEditText username;
+    private TextInputEditText password;
+    private TextInputEditText passwordagain;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_signup);
 
-        setUpParseUser();
+        progressDialog = new ProgressDialog(SignUpActivity.this);
+
+        back = findViewById(R.id.back);
+        signUp = findViewById(R.id.signup);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        passwordagain = findViewById(R.id.passwordagain);
+
+
+        signUp.setOnClickListener(v -> {
+            if (password.getText().toString().equals(passwordagain.getText().toString()) && !TextUtils.isEmpty(username.getText().toString()))
+                signUp(username.getText().toString(), password.getText().toString());
+            else
+                Toast.makeText(this, "Make sure that the values you entered are correct.", Toast.LENGTH_SHORT).show();
+        });
+
+        back.setOnClickListener(v -> finish());
+
     }
 
-    private void setUpParseUser() {
+    private void signUp(String username, String password) {
+        progressDialog.show();
         ParseUser user = new ParseUser();
         // Set the user's username and password, which can be obtained by a forms
-        user.setUsername( "<Your username here>");
-        user.setPassword( "<Your password here>");
-        user.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    showAlert("Successful Sign Up!", "Welcome" + "<Your username here>" +"!");
-                } else {
-                    ParseUser.logOut();
-                    Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                }
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(e -> {
+            progressDialog.dismiss();
+            if (e == null) {
+                showAlert("Successful Sign Up ! You logged in...\n", "Welcome " + username + " !");
+            } else {
+                ParseUser.logOut();
+                Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private void showAlert(String title,String message){
+    //TODO: Find a way to abstract showAlert
+    private void showAlert(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this)
                 .setTitle(title)
                 .setMessage(message)
--- app/src/main/java/com/example/crumbtrail/SplashActivity.java
+++ app/src/main/java/com/example/crumbtrail/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
 
         final Handler handler = new Handler();
         handler.postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            Intent intent = new Intent(SplashActivity.this, SignUpActivity.class);
             startActivity(intent);
             finish();
         }, 0); // TODO: For debugging purposes delay is set to 0, change to 3000 for v1
